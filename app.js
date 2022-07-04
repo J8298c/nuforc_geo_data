5674f301f9c79c11282a3609a4d5e9a271b5023d
Julio Mojica <juliocmojica@gmail.com>
2022-07-04 00:07:28 +0000

    extend express.js

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.post('/state/geojson', async (req, res) => {
+  try {
+    console.log('hey yo');
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+app.listen(PORT, () => console.log(`app is listening on port: ${PORT}`));
